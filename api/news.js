d4d216df1807b8ae3793c279e568e8d5b1baf16a
valentinbaldacci642-maker <valentinbaldacci642@gmail.com>
2026-05-02 11:12:27 +0200

    Fix RSS URLs: use Google News site: queries for L'Equipe, Maxifoot, Foot Mercato

--- api/news.js
+++ api/news.js
@@ -67,9 +67,9 @@ function parseItems(xml, source) {
 const FEEDS = [
   { url: 'https://rmcsport.bfmtv.com/rss/football/', source: 'RMC Sport' },
   { url: 'https://www.90min.com/fr/posts.rss', source: '90min' },
-  { url: 'https://www.lequipe.fr/rss/actu_rss_Football.xml', source: "L'Équipe" },
-  { url: 'https://www.maxifoot.fr/rss/rss.php', source: 'Maxifoot' },
-  { url: 'https://www.footmercato.net/flux-rss/rss.xml', source: 'Foot Mercato' },
+  { url: 'https://news.google.com/rss/search?q=site:lequipe.fr+football&hl=fr&gl=FR&ceid=FR:fr', source: "L'Équipe" },
+  { url: 'https://news.google.com/rss/search?q=site:maxifoot.fr&hl=fr&gl=FR&ceid=FR:fr', source: 'Maxifoot' },
+  { url: 'https://news.google.com/rss/search?q=site:footmercato.net&hl=fr&gl=FR&ceid=FR:fr', source: 'Foot Mercato' },
 ];
 
 module.exports = async function(req, res) {
--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -8,10 +8,9 @@ import { fr } from 'date-fns/locale';
 const FEEDS = [
   { url: 'https://rmcsport.bfmtv.com/rss/football/', source: 'RMC Sport' },
   { url: 'https://www.90min.com/fr/posts.rss', source: '90min' },
-  { url: 'https://www.lequipe.fr/rss/actu_rss_Football.xml', source: "L'Équipe" },
-  { url: 'https://www.maxifoot.fr/rss/rss.php', source: 'Maxifoot' },
-  { url: 'https://www.footmercato.net/flux-rss/rss.xml', source: 'Foot Mercato' },
-  { url: 'https://news.google.com/rss/search?q=football+ligue+1+france&hl=fr&gl=FR&ceid=FR:fr', source: 'Google News' },
+  { url: 'https://news.google.com/rss/search?q=site:lequipe.fr+football&hl=fr&gl=FR&ceid=FR:fr', source: "L'Équipe" },
+  { url: 'https://news.google.com/rss/search?q=site:maxifoot.fr&hl=fr&gl=FR&ceid=FR:fr', source: 'Maxifoot' },
+  { url: 'https://news.google.com/rss/search?q=site:footmercato.net&hl=fr&gl=FR&ceid=FR:fr', source: 'Foot Mercato' },
 ];
 
 const PROXY = 'https://api.allorigins.win/raw?url=';
